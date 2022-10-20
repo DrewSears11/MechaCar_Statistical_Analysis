1e324f794bae876323648d551849099238313f20
DrewSears11 <108240844+DrewSears11@users.noreply.github.com>
2022-10-20 11:30:59 -0400

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -11,3 +11,13 @@ Suspensioncoil_table <- read.csv(file='Suspension_Coil.csv',check.names=F,string
 summarize_PSI <- Suspensioncoil_table %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), variance_PSI=var(PSI), sd_PSI=sd(PSI),  .groups = 'keep')
 
 lotsummary <- Suspensioncoil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), variance_PSI=var(PSI), sd_PSI=sd(PSI),  .groups = 'keep')
+
+t.test(Suspensioncoil_table$PSI, mu=1500)
+
+t.test(subset(Suspensioncoil_table, Manufacturing_Lot=="Lot1")$PSI,mu=1500)
+
+t.test(subset(Suspensioncoil_table, Manufacturing_Lot=="Lot2")$PSI,mu=1500)
+
+t.test(subset(Suspensioncoil_table, Manufacturing_Lot=="Lot3")$PSI,mu=1500)
+
+colMeans(Mechacar_table)
